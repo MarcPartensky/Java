ca48ff195076f92603d42ddfb103812fb11c56e3
marc <marc.partensky@gmail.com>
2021-05-28 11:23:00 +0200

    makefile

---
+++ Test/test.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Arrays;
+
+// import java.util.LinkedList;
+// import java.util.Queue;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
