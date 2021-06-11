e5825ec1e083b2fb26b8f22e6d12bb6169fc5649
marc <marc.partensky@gmail.com>
2021-06-11 14:27:42 +0200

    char char chat

--- Test/test.java
+++ Test/test.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class Test {
-	static public String test() {
-		return String.valueOf(3.14);
+	static public char test() {
+		return 1 + 'a';
 	}
 	public static void main(String[] args) {
 		System.out.println(test());
